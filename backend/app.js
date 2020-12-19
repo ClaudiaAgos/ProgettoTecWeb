6ff35f0d88231bf562b3fbfd94120fba14579e18
fradepa <francescadepasquale0@gmail.com>
2020-12-19 16:43:45 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "angular")));
 
-app.use("/editor", )
 
 app.use((req, res, next) => {
   res.sendFile( path.join(__dirname,"angular", "index.html"));
