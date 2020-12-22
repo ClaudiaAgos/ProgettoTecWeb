f021a58a7069db253d71487fd0adabf7fd5c7faa
fradepa <francescadepasquale0@gmail.com>
2020-12-22 17:19:44 +0100

    New update

---
+++ src/app/player/team/team.component.ts
@@ -0,0 +1,20 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+
+
+
+export class TeamComponent {
+
+  DataButton = '';
+
+  onSubmit(){
+    this.DataButton = 'Iniziato';
+  }
+
+
+}
