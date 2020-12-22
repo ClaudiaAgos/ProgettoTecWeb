8a66ca774d0b58ff4a06aa365293143b74614100
fradepa <francescadepasquale0@gmail.com>
2020-12-22 20:45:13 +0100

    Test ide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 // @ts-ignore
+import { PlayerComponent } from './player/player.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
@@ -12,19 +13,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { EditorComponent } from './editor/editor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PlayerInitialComponent} from "./player/player-initial/player-initial.component";
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from "@angular/forms";
 import {MatRadioModule} from "@angular/material/radio";
 import {TeamComponent} from "./player/team/team.component";
 
-
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PlayerComponent,
     EditorComponent,
-    PlayerInitialComponent,
     TeamComponent
   ],
   imports: [
