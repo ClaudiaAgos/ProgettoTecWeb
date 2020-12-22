0ec5de454acd438ff9060c55c7571f42feb7f4ab
fradepa <francescadepasquale0@gmail.com>
2020-12-22 20:46:18 +0100

    Update 0.0.1

---
+++ src/app/player/player.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+
+// @ts-ignore
+@Component({
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.css']
+})
+
+export class PlayerComponent{}
